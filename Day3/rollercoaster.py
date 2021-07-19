5274d452d8c274e76e3c25afef282a0b1527211a
Nithish kumar M <nithishindians@gmail.com>
2021-07-19 21:30:51 +0530

    roller coaster is added

---
+++ Day3/rollercoaster.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster")
+height = int(input("Enter your height in cm?"))
+if height >120:
+    print("You are eligible to ride. Have a safe journey.")
+else:
+    print("You are not eligible. sorry better luck next time.")
